3eac93512dfab0bfe13123cb541fde1077f7c05f
Christos Paisios <cpaisios@gmail.com>
2016-07-10 13:52:18 +0300

    selection sort implemented 2.2-2

---
+++ SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    public class SelectionSort<T> where T : struct, IComparable<T>
+    {
+        /// <summary>
+        /// It sorts the elements of the input array in an increasing order
+        /// using the selection sort algortihm.
+        /// </summary>
+        /// <param name="elements"></param>
+        public void Run(T[] array)
+        {
+            if (array == null) throw new ArgumentException("array cannot be null");
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                int indexOfSmallest = i;
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[indexOfSmallest].CompareTo(array[j]) > 0)
+                    {
+                        indexOfSmallest = j;
+                    }
+                }
+                if (indexOfSmallest != i)
+                {
+                    var temp = array[i];
+                    array[i] = array[indexOfSmallest];
+                    array[indexOfSmallest] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ SortingAlgorithmsTests/SelectionSortTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SortingAlgorithms;
+using System;
+
+namespace SortingAlgorithmsTests
+{
+    [TestFixture]
+    public class SelectionSortTests
+    {
+        [Test]
+        public void SelectionSortAscendingTest()
+        {
+            var selectionSort = new SelectionSort<int>();
+            var array = new[] { 5, 2, 4, 7, 1, 3, 1, 6 };
+            selectionSort.Run(array);
+            Assert.IsTrue(array.IsSortedAscending());
+        }
+
+        [Test]
+        public void NullArraySelectionSortTest()
+        {
+            var selectionSort = new SelectionSort<int>();
+            int[] array = null;
+            Assert.Throws<ArgumentException>(() => selectionSort.Run(array));
+        }
+    }
+}
